99b7b691bb973bd986ce4b3ffab9b7e37045ebd2
AlexandreJr <juniordias_@live.com>
2024-12-04 14:51:26 -0300

    chore: add zustand

--- src/modules/auth/screens/SignInScreen/SignInScreen.tsx
+++ src/modules/auth/screens/SignInScreen/SignInScreen.tsx
@@ -3,15 +3,23 @@ import { Button, View } from "react-native";
 
 import { Container, Text } from "./styles";
 import { useNavigation } from "@react-navigation/native";
+import { useStore } from "../../../../store";
 
 export const SignInScreen = () => {
   const navigation = useNavigation();
 
+  const bears = useStore((state) => state.bears);
+  const increasePopulation = useStore((state) => state.increasePopulation);
+  const removeAllBears = useStore((state) => state.removeAllBears);
+  const updateBears = useStore((state) => state.updateBears);
+
   return (
     <Container>
-      <Text>Sign</Text>
+      <Text>Sign {bears}</Text>
 
-      <Button title="SIGN UP" onPress={() => navigation.navigate("SignUp")} />
+      <Button title="ADD NUMBER" onPress={() => increasePopulation()} />
+      <Button title="REMOVE" onPress={() => removeAllBears()} />
+      <Button title="UPDATE" onPress={() => updateBears(27)} />
     </Container>
   );
 };
